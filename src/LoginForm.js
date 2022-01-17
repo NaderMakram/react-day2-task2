059ab68a95029859afd9e68837ca97ee03282741
NaderMakram <nadermakram98@gmail.com>
2022-01-17 23:03:41 +0200

    first commit

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,10 +1,19 @@
 import { useState, useEffect } from "react";
 
 const LoginForm = () => {
-  const [userData, setUserData] = useState({ email: "", password: "" });
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    userName: "",
+    password: "",
+    confirmPassword: "",
+  });
   const [errors, setErrors] = useState({
+    nameError: "",
     emailError: "",
+    userNameError: "",
     passwordError: "",
+    confirmPasswordError: "",
   });
 
   // set the value
@@ -12,6 +21,31 @@ const LoginForm = () => {
     const { name, value } = e.target;
     setUserData({ ...userData, [name]: value });
   };
+
+  // name validation
+  useEffect(() => {
+    if (userData.name === "") {
+      setErrors({ ...errors, nameError: "this field is required" });
+    } else {
+      setErrors({ ...errors, nameError: "" });
+    }
+  }, [userData.name]);
+
+  // user name validation
+  useEffect(() => {
+    if (userData.userName === "") {
+      setErrors({ ...errors, userNameError: "this field is required" });
+    } else {
+      if (!validateUserName(userData.userName)) {
+        setErrors({
+          ...errors,
+          userNameError: "please remove any spaces",
+        });
+      } else {
+        setErrors({ ...errors, userNameError: "" });
+      }
+    }
+  }, [userData.userName]);
 
   // email validation
   useEffect(() => {
@@ -34,10 +68,10 @@ const LoginForm = () => {
     if (userData.password === "") {
       setErrors({ ...errors, passwordError: "this field is required" });
     } else {
-      if (userData.password.length < 8) {
+      if (!validatePassword(userData.password)) {
         setErrors({
           ...errors,
-          passwordError: "please choose a password with 8 characters or more",
+          passwordError: "please choose a password with 8 characters or more that has one number, one special character",
         });
       } else {
         setErrors({ ...errors, passwordError: "" });
@@ -45,10 +79,21 @@ const LoginForm = () => {
     }
   }, [userData.password]);
 
-  const handleSubmit = (e)=>{
-    e.preventDefault()
-  }
+  // confirm password validation
+  useEffect(() => {
+    if (userData.confirmPassword !== userData.password) {
+      setErrors({ ...errors, confirmPasswordError: "please make sure passwords match" });
+    } else {
+      setErrors({ ...errors, confirmPasswordError: "" });
+    }
+  }, [userData.confirmPassword]);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+
+  // validation helper functions
   const validateEmail = (email) => {
     return String(email)
       .toLowerCase()
@@ -56,18 +101,39 @@ const LoginForm = () => {
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
       );
   };
+  const validatePassword = (password) => {
+    return String(password).match(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+    );
+  };
+  const validateUserName = (name) => {
+    return String(name).match(/^\S*$/);
+  };
 
   return (
     <>
-    
       <form className="custom-container" onSubmit={handleSubmit}>
+        {/* name */}
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Name</label>
+          <input
+            type="text"
+            className="form-control"
+            name="name"
+            value={userData.name}
+            onChange={handleChange}
+          />
+          <small id="error" className="form-text text-muted">
+            {errors.nameError}
+          </small>
+        </div>
+        {/* email */}
         <div className="form-group">
           <label htmlFor="exampleInputEmail1">Email address</label>
           <input
             type="email"
             className="form-control"
             id="exampleInputEmail1"
-            placeholder="Enter email"
             name="email"
             value={userData.email}
             onChange={handleChange}
@@ -76,13 +142,27 @@ const LoginForm = () => {
             {errors.emailError}
           </small>
         </div>
+        {/* user name */}
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">User Name</label>
+          <input
+            type="text"
+            className="form-control"
+            name="userName"
+            value={userData.userName}
+            onChange={handleChange}
+          />
+          <small id="error" className="form-text text-muted">
+            {errors.userNameError}
+          </small>
+        </div>
+        {/* password */}
         <div className="form-group">
           <label htmlFor="exampleInputPassword1">Password</label>
           <input
             type="password"
             className="form-control"
             id="exampleInputPassword1"
-            placeholder="Password"
             name="password"
             value={userData.password}
             onChange={handleChange}
@@ -91,8 +171,23 @@ const LoginForm = () => {
             {errors.passwordError}
           </small>
         </div>
+        {/* confirm password */}
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Confirm Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            name="confirmPassword"
+            value={userData.confirmPassword}
+            onChange={handleChange}
+          />
+          <small id="error" className="form-text text-muted">
+            {errors.confirmPasswordError}
+          </small>
+        </div>
         <button type="submit" className="btn btn-primary">
-          Login
+          Register
         </button>
       </form>
     </>
